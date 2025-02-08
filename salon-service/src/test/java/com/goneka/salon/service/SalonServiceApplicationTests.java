3318850581ed9eb29d68752cda569046e6d4335d
GladmanRisolvr <gnyamugoneka@risolvr.com>
2025-02-08 04:48:45 +0200

    Moved User Service to Parent Microservice Directory

---
+++ salon-service/src/test/java/com/goneka/salon/service/SalonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goneka.salon.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
